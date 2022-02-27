b17c7576295e2299390d3e79316cf223f46a1c81
Ramyuser <89673479+Ramyuser@users.noreply.github.com>
2022-02-26 18:23:18 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,64 @@
+function pigeon() {
+    animal_animal = document.getElementById("animal_name").value;
+    window.location = "animal.html";
+    localStorage.setItem("animal_name", animal_name);
+}
+
+function rhinoceros() {
+    animal_animal = document.getElementById("animal_name").value;
+    window.location = "rhinoceros.html";
+    localStorage.setItem("animal_name", animal_name);
+}
+
+function dolphin() {
+    animal_animal = document.getElementById("animal_name").value;
+    window.location = "dolphin.html";
+    localStorage.setItem("animal_name", animal_name);
+}
+
+function Ibex() {
+    animal_animal = document.getElementById("animal_name").value;
+    window.location = "ibex.html";
+    localStorage.setItem("animal_name", animal_name);
+}
+
+function cat() {
+    animal_animal = document.getElementById("animal_name").value;
+    window.location = "cat.html";
+    localStorage.setItem("animal_name", animal_name);
+}
+
+function tiger() {
+    animal_animal = document.getElementById("animal_name").value;
+    window.location = "tiger.html";
+    localStorage.setItem("animal_name", animal_name);
+}
+
+function notCow() {
+    animal_animal = document.getElementById("animal_name").value;
+    window.location = "notcow.html";
+    localStorage.setItem("animal_name", animal_name);
+}
+
+function auk() {
+    animal_animal = document.getElementById("animal_name").value;
+    window.location = "auk.html";
+    localStorage.setItem("animal_name", animal_name);
+}
+
+function dodo() {
+    animal_animal = document.getElementById("animal_name").value;
+    window.location = "dodo.html";
+    localStorage.setItem("animal_name", animal_name);
+}
+
+function mammoth() {
+    animal_animal = document.getElementById("animal_name").value;
+    window.location = "mammoth.html";
+    localStorage.setItem("animal_name", animal_name);
+}
+
+function back() {
+    localStorage.removeItem("animal_name");
+    window.location = "welcome.html";
+}
